c048a0770186846139e6ce6e17e9cdc5bc66d269
UmutDuman-41 <Uduman0629@gmail.com>
2026-06-07 20:35:26 +0300

    a

--- Metotlar/SepetManager.cs
+++ Metotlar/SepetManager.cs
@@ -11,7 +11,6 @@ namespace Metotlar
         public void Ekle(Urun urun) // Ne eklemek istediğimizi parantez içinde metoda(fonksiyon da denebilir) verdik.'Urun' veri tipi; küçük urun ise kullanırken verdiğimiz isimlendirme.
         {
             Console.WriteLine("Tebrikler.Sepete Eklendi : " + urun.Adi);
-
         }
 
         public void Ekle2(string urunAdi,string aciklama, double fiyat,int stokAdedi) // uzun uzun yazmaktansa Urun clasına stokAdedini ekleyerek işimizi kolaylaştırırız.
